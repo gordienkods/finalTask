26d962dd9f1ecc4aa91f79ee2cada4a76ca63d7b
gordienkods <gordienkods@gmail.com>
2014-12-24 23:49:43 +0200

    log4j settings

--- src/test/java/navigator/AnyWebDriver.java
+++ src/test/java/navigator/AnyWebDriver.java
@@ -6,25 +6,35 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.apache.log4j.Logger;
 
 public class AnyWebDriver {
-
+    private static final Logger logWarn = Logger.getLogger(AnyWebDriver.class);
+    private static final Logger logInfo = Logger.getLogger(AnyWebDriver.class);
+    private static final Logger logErr = Logger.getLogger(AnyWebDriver.class);
     public WebDriver getWebDriver (){
         WebDriver driver;
         String anyWebDriver = System.getProperty("anyWebDriver");
         if ("chromedriver".equals(anyWebDriver)){
             try{
-                System.setProperty("webdriver.chrome.driver", "C:/Program Files (x86)/QA tools/chromedriver.exe");
+                System.setProperty("webdriver.chrome.driver", ":/Program Files (x86)/QA tools/chromedriver.exe");
+                logInfo.info("Starting Chrome...");
                 driver = new ChromeDriver();
+                logInfo.info("Chrome started successfuly.");
                 return driver;
             } catch (IllegalStateException e){
+                logWarn.warn("Chrome start failed!");
+                logInfo.info("Starting FireFox...");
                 driver = new FirefoxDriver();
+                logInfo.info("FireFox started successfuly.");
                 return driver;
             }
         }
         if ("firefox".equals(anyWebDriver)){
             try{
+                logInfo.info("Starting FireFox...");
                 driver = new FirefoxDriver();
+                logInfo.info("FireFox started successfuly.");
                 return driver;
             } catch (IllegalStateException e){
+                logErr.error("FireFox started failed!");
                 return null;
             }
         }
